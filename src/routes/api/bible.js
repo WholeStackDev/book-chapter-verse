a2a66462d02f00233664fe1bc4fd6b1d306f7728
Michael Phillips <wholestackdev@gmail.com>
2022-05-14 19:22:58 -0400

    Added caching on the server to prevent compressing the bible every time

--- src/routes/api/bible.js
+++ src/routes/api/bible.js
@@ -1,9 +1,15 @@
 import { data } from '$lib/data/web';
 import lz from 'lz-string';
+import NodeCache from 'node-cache';
+const serverCache = new NodeCache();
 
 export async function get() {
 	try {
-		const compressedData = lz.compressToUTF16(JSON.stringify(data));
+		var compressedData = serverCache.get('web');
+		if (compressedData == undefined) {
+			compressedData = lz.compressToUTF16(JSON.stringify(data));
+			serverCache.set('web', compressedData);
+		}
 		return {
 			body: compressedData
 		};
