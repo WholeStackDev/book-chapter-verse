d3d896eb3f44dabe7085714f79b1da43bacfa8a2
Michael Phillips <wholestackdev@gmail.com>
2022-05-14 17:19:35 -0400

    Still trying to get vercel to be able to access web.json

--- src/routes/api/bible.js
+++ src/routes/api/bible.js
@@ -2,13 +2,13 @@ import fs from 'fs';
 
 export async function get() {
 	try {
-		const data = fs.readFileSync('web.json');
+		const data = fs.readFileSync('./web.json');
 
 		return {
 			body: data
 		};
 	} catch (error) {
-		log(error);
+		console.log(error);
 		return {
 			status: 500
 		};
