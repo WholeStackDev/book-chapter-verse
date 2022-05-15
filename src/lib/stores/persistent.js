64f9f87a0cbf133f901a56dd73535fecd4751b13
Michael Phillips <wholestackdev@gmail.com>
2022-05-14 20:06:56 -0400

    Trying storing the compressed bible as a static file to load

--- src/lib/stores/persistent.js
+++ src/lib/stores/persistent.js
@@ -10,10 +10,29 @@ if (browser) {
 		if (await db.getItem('bible')) {
 			bible.set(JSON.parse(await db.getItem('bible')));
 		} else {
-			const data = await fetch('/api/bible');
-			const compressedData = await data.text();
-			const uncompressed = lz.decompressFromUTF16(compressedData);
-			bible.set(JSON.parse(uncompressed));
+			try {
+				const data = await fetch(`../compressed.txt`);
+				const compressedData = await data.text();
+				const uncompressed = lz.decompressFromUTF16(compressedData);
+				bible.set(JSON.parse(uncompressed));
+				console.log('first method worked');
+			} catch (error) {
+				console.log(error);
+				try {
+					const data = await fetch(`/compressed.txt`);
+					const compressedData = await data.text();
+					const uncompressed = lz.decompressFromUTF16(compressedData);
+					bible.set(JSON.parse(uncompressed));
+					console.log('second method worked');
+				} catch (error) {
+					console.log(error);
+					const data = await fetch('/api/bible');
+					const compressedData = await data.text();
+					const uncompressed = lz.decompressFromUTF16(compressedData);
+					bible.set(JSON.parse(uncompressed));
+					console.log('third method worked');
+				}
+			}
 		}
 
 		bible.subscribe((x) => db.setItem('bible', JSON.stringify(x)));
--- src/routes/api/compress.js
+++ src/routes/api/compress.js
@@ -1,9 +1,11 @@
 import { data } from '$lib/data/web';
 import lz from 'lz-string';
+import fs from 'fs';
 
 export async function get() {
 	try {
-		const compressedData = lz.compressToUTF16(JSON.stringify(data));
+		compressedData = lz.compressToUTF16(JSON.stringify(data));
+		fs.writeFileSync('compressed.txt', compressedData);
 		return {
 			body: compressedData
 		};
